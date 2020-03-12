b5b6d69d225050f36006a1216d2e4c24c91bd069
bodhisha <bodhisha@gmail.com>
2020-03-12 22:11:32 +0530

    input validation

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-let qrcode = select("img");
-let qrtext = select("textarea");
-let qrbtn = select("button");
+//let qrcode = select("img");
+//let qrtext = select("textarea");
+//let qrbtn = select("button");
 
-qrbtn.addEventListener("click", generateQR);
+//qrbtn.addEventListener("click", return validate && generateQR);
+
+function validate() {
+  var text = document.getElementById("text");
+  if (text.value == "") {
+    alert("Text Box cannot be empty");
+    return false;
+  } else {
+    return true;
+  }
+}
 
 function generateQR() {
+  let qrcode = select("img");
+  let qrtext = select("textarea");
+  let qrbtn = select("button");
   let size = "1000x1000";
   let data = qrtext.value;
   let baseURL = "http://api.qrserver.com/v1/create-qr-code/";
